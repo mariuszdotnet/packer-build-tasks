bbb23f0c52b5d27f4bd505d4c5c03ec7a49a2668
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-12 01:21:54 -0400

    updated to bash

--- packer-win-vhd-build.sh
+++ packer-win-vhd-build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo 'List root directory content'
 ls -al
-cd packer-templates
+cp secrets/packer-secrets.json packer-templates/vhd-disk/packer-secrets.json
+cd packer-templates/vhd-disk
 ls -al
 
 #packer validate  -var-file="variables.json" windows2016.json
