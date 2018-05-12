93e12c2879bb6cce2a871f5dfa457f9f35a9d18d
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-12 00:40:30 -0400

    empty task

---
+++ packer-win-vhd-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#cd packer_windows2016
+
+ls -al
+cd packer-templates
+ls -al
+
+#packer validate  -var-file="variables.json" windows2016.json
+#packer build  -var-file="variables.json" windows2016.json
