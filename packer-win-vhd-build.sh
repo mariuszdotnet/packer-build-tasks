b21ca9ac2183e1ad1f21634f494c87922a9430f7
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-12 01:29:23 -0400

    updated to bash

--- packer-win-vhd-build.sh
+++ packer-win-vhd-build.sh
@@ -2,10 +2,18 @@
 
 echo 'List root directory content'
 ls -al
+
+echo 'Copy secrets to packer template directory'
 cp secrets/packer-secrets.json packer-templates/vhd-disk/packer-secrets.json
+
+echo 'Change to packer template directory'
 cd packer-templates/vhd-disk
+
+echo 'List packer template directory content'
 ls -al
+
+echo 'Start packer validate'
 packer validate -var-file="variables.json" -var-file="packer-secrets.json" windows.json
 
-#packer validate  -var-file="variables.json" windows2016.json
-#packer build  -var-file="variables.json" windows2016.json
+echo 'start packer build'
+packer build -var-file="variables.json" -var-file="packer-secrets.json" windows.json
