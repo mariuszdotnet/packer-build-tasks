c646586226ea325fc39dabb3214d9200d7cacc81
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-12 01:23:54 -0400

    updated to bash

--- packer-win-vhd-build.sh
+++ packer-win-vhd-build.sh
@@ -5,6 +5,7 @@ ls -al
 cp secrets/packer-secrets.json packer-templates/vhd-disk/packer-secrets.json
 cd packer-templates/vhd-disk
 ls -al
+packer validate -var-file="variables.json" -var-file="packer-secrets.json" windows.json
 
 #packer validate  -var-file="variables.json" windows2016.json
 #packer build  -var-file="variables.json" windows2016.json
