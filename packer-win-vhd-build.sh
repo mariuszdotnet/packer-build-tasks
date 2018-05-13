a2908dcc684b3f8a82ead5f84c93fa36a5529447
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-12 23:55:43 -0400

    testing

--- packer-win-vhd-build.sh
+++ packer-win-vhd-build.sh
@@ -2,25 +2,26 @@
 
 #echo 'TEMP STUFF'
 #cd /
+ls -al
 git clone packer-build-tasks output-manifest
 cd output-manifest
 ls -al
-cd ..
+cd /
 
-echo 'List root directory content'
-ls -al
+#echo 'List root directory content'
+#ls -al
 
-echo 'Copy secrets to packer template directory'
-cp secrets/packer-secrets.json packer-templates/vhd-disk/packer-secrets.json
+#echo 'Copy secrets to packer template directory'
+#cp secrets/packer-secrets.json packer-templates/vhd-disk/packer-secrets.json
 
-echo 'Change to packer template directory'
-cd packer-templates/vhd-disk
+#echo 'Change to packer template directory'
+#cd packer-templates/vhd-disk
 
-echo 'List packer template directory content'
-ls -al
+#echo 'List packer template directory content'
+#ls -al
 
-echo 'Start packer validate'
-packer validate -var-file="variables.json" -var-file="packer-secrets.json" windows.json
+#echo 'Start packer validate'
+#packer validate -var-file="variables.json" -var-file="packer-secrets.json" windows.json
 
 #echo 'start packer build'
 #packer build -var-file="variables.json" -var-file="packer-secrets.json" windows.json
