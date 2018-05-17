8c39a36381e287256b245e5a59a94f2a1d3440e6
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-16 23:02:24 -0400

    updating vars

--- azcli-vhd-copy.sh
+++ azcli-vhd-copy.sh
@@ -32,8 +32,47 @@ dest_vhd_storage_account_rg=$(echo $targer_region_json | jq -r '.vhd_storage_acc
 dest_vhd_storage_account_name=$(echo $targer_region_json | jq -r '.vhd_storage_account_name')
 dest_vhd_uri=$(echo $targer_region_json | jq -r '.vhd_uri')
 
-printenv
-
+#printenv
 #dest_image_rg='ImagesRepo'
 #dest_image_name='MM_MK_Test3'
 #dest_image_os_type='Windows'
+
+
+#Authenticate with service principal
+echo "Authenticating to Azure with service principal $servicePrincipal"
+echo "and pwd $servicePrincipalPwd"
+az login --service-principal -u "$servicePrincipal" --password "$servicePrincipalPwd" --tenant "microsoft.onmicrosoft.com"
+echo 'Azure login completed'
+az account set --subscription $subscriptionId
+#
+#TODO: Confirm if access keys are even needed - appears to work without
+#Get storage account access keys
+sourceStorageAccountKey=$(az storage account keys list -g $vhd_storage_account_rg --account-name $vhd_storage_account_name --query "[:1].value" -o tsv)
+targetStorageAccountKey=$(az storage account keys list -g $dest_vhd_storage_account_rg --account-name $dest_vhd_storage_account_name --query "[:1].value" -o tsv)
+#
+#Start blob copy
+dest_image_src_URI="https://$dest_vhd_storage_account_name.blob.core.windows.net/$vhd_storage_container/$dest_vhd_uri"
+blobCopyStatus=$(az storage blob show --container-name $vhd_storage_container -n $dest_vhd_uri --account-name $dest_vhd_storage_account_name --query "properties.copy.status")
+echo "Current blob copy status: $blobCopyStatus"
+#Blob statuses: "pending", "success"
+#Check if copy operation in progress
+if [ $blobCopyStatus == "\"pending\"" ]
+then
+  echo "Copy operation already in progress to $dest_image_src_URI. Switching to monitoring"
+else
+  echo "Starting blob copy operation from $vhd_uri to $dest_image_src_URI"
+  copyId=$(az storage blob copy start --source-uri $vhd_uri --destination-blob $dest_vhd_uri --destination-container $vhd_storage_container --account-name $dest_vhd_storage_account_name )
+    #Appears that storage account key is not required for auth
+    #az storage blob copy start --source-uri $vhd_uri --destination-blob $dest_vhd_uri --destination-container $vhd_storage_container --account-name $dest_vhd_storage_account_name --account-key $targetStorageAccountKey --source-account-key $sourceStorageAccountKey
+fi
+
+#Wait for blob copy to complete
+while [  $blobCopyStatus != "\"success\"" ]
+do
+    echo "Blob copy in progress. Bytes copied $(az storage blob show --container-name $vhd_storage_container -n $dest_vhd_uri --account-name $dest_vhd_storage_account_name --query "properties.copy.progress")"
+    sleep 60
+    blobCopyStatus=$(az storage blob show --container-name $vhd_storage_container -n $dest_vhd_uri --account-name $dest_vhd_storage_account_name --query "properties.copy.status")
+done
+
+echo "Blob copy completed"     
+az storage blob show --container-name $vhd_storage_container -n $dest_vhd_uri --account-name $dest_vhd_storage_account_name --query "properties.copy.status"
