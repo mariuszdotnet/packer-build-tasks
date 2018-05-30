90727503290bfc9351fd6d3cd9142e908d67efee
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-30 13:18:32 -0400

    added image_create task

---
+++ azcli-image-create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+### VM image file cross-region distrubution and image creation
+### Date: May 11, 2018
+
+# Copy 'manifest.json' into working directory 'packer-build-tasks'
+echo 'Hello World!'
