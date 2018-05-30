9b5ed3a3537efc9ec780cf936e6f37bad55276ae
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-30 15:17:28 -0400

    updates to image create

--- azcli-image-create.sh
+++ azcli-image-create.sh
@@ -3,4 +3,9 @@
 ### Date: May 11, 2018
 
 # Copy 'manifest.json' into working directory 'packer-build-tasks'
-echo 'Hello World!'
+echo 'Copy manifestjson into packer-build-tasks'
+cp packer-build-output/manifest.json packer-build-tasks/manifest.json
+
+echo 'Change to packer template directory and list content'
+cd packer-build-tasks
+ls -al
