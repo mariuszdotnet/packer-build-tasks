93107ed1b91afdb5803e326cf32283d03018ac16
Mariusz Kolodziej <makolo@microsoft.com>
2018-05-30 21:41:51 -0400

    work on image copy

--- azcli-image-create.sh
+++ azcli-image-create.sh
@@ -9,3 +9,54 @@ cp packer-build-output/manifest.json packer-build-tasks/manifest.json
 echo 'Change to packer template directory and list content'
 cd packer-build-tasks
 ls -al
+
+# Set all environment variables from image_copy_config.json, manifest.json and TASK ENV variables
+echo 'Load all required environment variables for VHD copy to desired subscription in targer region'
+
+config_file='image_copy_config.json'
+servicePrincipal=$servicePrincipal
+servicePrincipalPwd=$servicePrincipalPwd
+
+# Set source variables
+source_region_json=$(jq --arg regionLocation $regionLocation '.region_location[] | select(any(.location; . == $regionLocation))' $config_file)
+vhd_name=$(echo $source_region_json | jq -r '.vhd_uri')
+subscriptionId=$(echo $source_region_json | jq -r '.subscription_id')
+vhd_storage_account_rg=$(echo $source_region_json | jq -r '.vhd_storage_account_rg')
+vhd_storage_account_name=$(echo $source_region_json | jq -r '.vhd_storage_account_name')
+vhd_storage_container=$(echo $source_region_json | jq -r '.vhd_storage_container')
+
+#Authenticate with service principal
+echo "Authenticating to Azure with service principal $servicePrincipal"
+#echo "and pwd $servicePrincipalPwd"
+az login --service-principal -u "$servicePrincipal" --password "$servicePrincipalPwd" --tenant "microsoft.onmicrosoft.com"
+echo 'Azure login completed'
+az account set --subscription $subscriptionId
+
+#Get source storage account access keys
+sourceStorageAccountKey=$(az storage account keys list -g $vhd_storage_account_rg --account-name $vhd_storage_account_name --query "[:1].value" -o tsv)
+
+#Iterate over all subscription strorage accounts in a region.  Copy VND and create image
+#mk=$(jq --arg regionLocation 'eastus2' '.region_location[] | select(any(.location; . == $regionLocation))' manifest.json)
+target_region_json=$(jq --arg regionLocation $regionLocation '.region_location[] | select(any(.location; . == $regionLocation))' $config_file)
+subscriptions=$(echo $target_region_json | jq -r '.subscription')
+
+for row in $(echo $subscriptions | jq -r '.[] | @base64'); do
+        _jq() {
+                echo $row | base64 --decode | jq -r ${1}
+        }
+
+        echo $(_jq '.subscription_id')
+done
+
+
+#Get target storage account access keys
+#targetStorageAccountKey=$(az storage account keys list -g $dest_vhd_storage_account_rg --account-name $dest_vhd_storage_account_name --query "[:1].value" -o tsv)
+
+# Set target region location specific variables for VHD copy
+#target_region_json=$(jq --arg regionLocation $regionLocation '.region_location[] | select(any(.location; . == $regionLocation))' $config_file)
+#location=$(echo $target_region_json | jq -r '.location')
+#dest_vhd_storage_container=$(echo $target_region_json | jq -r '.vhd_storage_container')
+#dest_subscriptionId=$(echo $target_region_json | jq -r '.subscription_id')
+#dest_vhd_storage_account_rg=$(echo $target_region_json | jq -r '.vhd_storage_account_rg')
+#dest_vhd_storage_account_name=$(echo $target_region_json | jq -r '.vhd_storage_account_name')
+#dest_vhd_uri=$(echo $target_region_json | jq -r '.vhd_uri')
